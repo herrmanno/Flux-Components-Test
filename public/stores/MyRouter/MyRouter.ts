f6f0fd66027f43fa78f1fd879ea7c65a4262af1a
herrmanno <O.herrmann92@gmail.com>
2015-08-28 13:16:21 +0200

    minor changes
    changed to Myrouter basing on Router(now on ho.ui)

--- public/stores/MyRouter/MyRouter.ts
+++ public/stores/MyRouter/MyRouter.ts
@@ -1,4 +1,11 @@
-class MyRouter extends ho.flux.Router {
+/// <reference path="../../typing.d.ts"/>
+
+class MyRouter extends Router {
+
+	constructor() {
+		super();
+		this.on('STATE', this.onStateChangeRequested.bind(this));
+	}
 
 	@ho.flux.Store.on(CONST.LOGIN_OK)
 	onLogin_OK() {
@@ -6,4 +13,36 @@ class MyRouter extends ho.flux.Router {
 		let data = {state: 'private', args: null, extern: false};
 		this.onStateChangeRequested(data);
 	}
+
+	mapping =  [
+		{
+			name: 'home',
+			url: 'home',
+			view: [
+				{name: 'view1', html: '<h3>Home</h3>'}
+			]
+		},
+		{
+			name: 'login',
+			url: 'login',
+			view: [
+				{name: 'view1', html: 'html/login.html'}
+			]
+		},
+		{
+			name: 'private',
+			url: 'private',
+			before: data => {
+				return ho.flux.STORES.get(LoginStore).check(data);
+			},
+			view: [
+				{ name: 'view1', html: '<p>PRIVATE! Go away!</p>' }
+			]
+		},
+		{
+			name: 'catchall',
+			url: '.*',
+			redirect: 'home'
+		}
+	];
 }
